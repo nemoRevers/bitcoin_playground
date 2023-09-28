0c499bdc830d5a6338273115f87716923fe5cd2e
Revers <sanyarevers@gmail.com>
2023-09-28 15:22:30 +0300

    refactored for getting strings pubKey privKey

--- lib/bitcoin_playground.dart
+++ lib/bitcoin_playground.dart
@@ -1,26 +1,24 @@
+import 'dart:convert';
 import 'dart:typed_data';
 
-import 'package:bip32/bip32.dart';
 import 'package:bip39/bip39.dart';
 import 'package:flutter_bitcoin/flutter_bitcoin.dart';
 
 void main() async {
   final mnemonic = generateMnemonic();
-  print(mnemonic);
   final seed = mnemonicToSeed(mnemonic);
   final index = 0;
   final address = await generateReceivingAddress(seed, index);
   print(address);
-  // print(wallet.pubKey);
-  // print(wallet.privKey);
-  // print(wallet.wif);
 }
 
 Future<String?> generateReceivingAddress(Uint8List seed, int index) async {
-  final root = BIP32.fromSeed(seed);
-  final node = root.derivePath("m/49'/0'/0'/0/$index");
-
-  final address =
-      P2WPKH(data: PaymentData(pubkey: node.publicKey)).data.address;
+  final wallet = HDWallet.fromSeed(seed).derivePath("m/49'/0'/0'/0/$index");
+  print(wallet.pubKey);
+  print(wallet.privKey);
+  final address = P2WPKH(
+          data: PaymentData(pubkey: utf8.encode(wallet.pubKey!) as Uint8List))
+      .data
+      .address;
   return address;
 }
