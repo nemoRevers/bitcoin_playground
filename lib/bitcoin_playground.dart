c7191631abb71d57c514915cb571e97f1df049a0
Revers <sanyarevers@gmail.com>
2023-09-29 13:15:28 +0300

    testnet

--- lib/bitcoin_playground.dart
+++ lib/bitcoin_playground.dart
@@ -5,7 +5,8 @@ import 'package:bip39/bip39.dart';
 import 'package:flutter_bitcoin/flutter_bitcoin.dart';
 
 void main() async {
-  final mnemonic = generateMnemonic();
+  final mnemonic = generateMnemonic(strength: 256);
+  print(mnemonic);
   final seed = mnemonicToSeed(mnemonic);
   final index = 0;
   final address = await generateReceivingAddress(seed, index);
@@ -13,12 +14,13 @@ void main() async {
 }
 
 Future<String?> generateReceivingAddress(Uint8List seed, int index) async {
-  final wallet = HDWallet.fromSeed(seed).derivePath("m/49'/0'/0'/0/$index");
+  final wallet = HDWallet.fromSeed(seed, network: testnet)
+      .derivePath("m/84'/1'/0'/0/$index");
   print(wallet.pubKey);
   print(wallet.privKey);
   final address = P2WPKH(
-          data: PaymentData(pubkey: utf8.encode(wallet.pubKey!) as Uint8List))
-      .data
-      .address;
+    data: PaymentData(pubkey: utf8.encode(wallet.pubKey!) as Uint8List),
+    network: wallet.network,
+  ).data.address;
   return address;
 }
